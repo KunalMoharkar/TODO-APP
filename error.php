c3d547f6f41e40a12b107c982969b0150c85dc11
KunalMoharkar <kunalmoharkar1700@gmail.com>
2020-11-03 17:13:59 +0530

    error handler

--- error.php
+++ error.php
@@ -1,4 +1,9 @@
 <?php require('header.php'); ?>
-
+    <div class="container">
+        <div class="alert alert-danger">
+            <h2>INTERNAL SERVER ERROR - 500</h2>
+            <p>Incorrect username or password</p>
+        </div>
+    </div>
 <?php require('footer.php'); ?>
 
